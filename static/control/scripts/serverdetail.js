c05653ec14e08643f12b60551a3c2c51f3860316
master <375272990@qq.com>
2018-11-13 13:54:53 +0800

    2018/11/13

--- static/control/scripts/serverdetail.js
+++ static/control/scripts/serverdetail.js
@@ -45,11 +45,11 @@ var FileTableInit = function () {
                 if (row.stress_test == "running") {
                     strclass = 'success';//还有一个active
                 }
-                else if (row.stress_test == 75) {
-                    strclass = 'warning';
+                else if (row.stress_test == "wait") {
+                    strclass = 'active';
                 }
                 else {
-                    strclass = 'warning'
+                    strclass = 'info'
                 }
                 return {classes: strclass}
             },
